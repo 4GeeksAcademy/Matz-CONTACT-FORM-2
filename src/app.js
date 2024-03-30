22912bd94502bc75cc2f63969fe73d5048b34e45
Matz72 <156270489+Matz72@users.noreply.github.com>
2024-03-30 20:38:26 +0000

    save changes

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   emailjs.init({
-    publicKey: "7FpxcV1iqROSRo9wx"
+    publicKey: "7FpxcV1iqROSRo9wx",
   });
+
+  const button = document.querySelector("#send-info");
+  button.addEventListener("click", sendEmails);
 };
+
+function sendEmails() {
+  alert("sending emails");
+}
