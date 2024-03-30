6857b43f174f728c86cf9b23b69ba096e0c711c0
Matz72 <156270489+Matz72@users.noreply.github.com>
2024-03-30 21:08:57 +0000

    save changes

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   emailjs.init({
-    publicKey: "7FpxcV1iqROSRo9wx",
+    publicKey: "7FpxcV1iqROSRo9wx"
   });
 
   const button = document.querySelector("#send-info");
@@ -19,4 +19,15 @@ function sendEmails() {
   const name = document.querySelector("#name-input").value;
   const email = document.querySelector("#email-input").value;
   const message = document.querySelector("#message-input").value;
+
+  emailjs.send("service_qyizc2s", "template_68nimpt", {
+    to_name: name,
+    to_email: email
+  });
+
+  emailjs.send("service_qyizc2s", "template_tiixzwa", {
+    from_name: name,
+    from_email: email,
+    message: message
+  });
 }
